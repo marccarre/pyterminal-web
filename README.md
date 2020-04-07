a166c72592b612eed2d7b30539ffda938c599a2a
Marc Carré <carre.marc@gmail.com>
2020-04-07 15:04:14 +0900

    Upgrade to Python 3

--- README.md
+++ README.md
@@ -2,7 +2,42 @@ pyterminal-web
 ==============
 
 An interactive Python terminal running as a webpage.
-
 Demo available [here](http://pyterminal-web.herokuapp.com/ "Demo page")!
 
-To run it locally: `python index.py localhost 1337`
+# Development
+
+## Local
+
+### Pre-requisites
+
+- Python 3
+- `pip`
+
+### Setup
+
+```console
+$ pip install --upgrade pipenv
+$ pipenv shell
+$ pipenv install
+```
+
+### Run
+
+```console
+$ python index.py 0.0.0.0 1337
+Bottle v0.11.6 server starting up (using WSGIRefServer())...
+Listening on http://0.0.0.0:1337/
+Hit Ctrl-C to quit.
+```
+
+## Heroku
+
+Heroku requires a `requirements.txt` file for Python applications' dependencies.
+To generate it using `pipenv`, run the following:
+
+```console
+$ pipenv install
+Installing dependencies from Pipfile.lock (ef4847)…
+  🐍   ▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉ 1/1 — 00:00:00
+$ pipenv lock --requirements > requirements.txt
+```
--- index.py
+++ index.py
@@ -1,80 +1,119 @@
+#!/usr/bin/env python
 import sys
 import traceback
-from StringIO import StringIO
+from io import StringIO
 from bottle import route, run, template, view, get, post, request
+
 
 @get('/')
 @post('/')
 @view('index')
 def index():
     reset = request.forms.get('reset', '').lower()
-    if (reset == 'reset'): resetTerminal()
+    if reset == 'reset':
+        reset_terminal()
+    command = request.forms.get('command', DEFAULT_COMMAND)
+    return {
+        'output': run_python(command if command.strip() else DEFAULT_COMMAND),
+    }
 
-    command = request.forms.get('command', defaultCommand)
-    output = runPython(command if (command.strip() != '') else defaultCommand)
-    return dict(output=output)
 
 @get('/sources')
 @view('sources')
 def sources():
-    python = readSources('index.py')
-    header = readSources('header.tpl')
-    index = readSources('index.tpl')
-    sources = readSources('sources.tpl')
-    footer = readSources('footer.tpl')
-    return dict(python=python,header=header,index=index,sources=sources,footer=footer)
+    return {
+        'python': read_sources('index.py'),
+        'header': read_sources('header.tpl'),
+        'index': read_sources('index.tpl'),
+        'sources': read_sources('sources.tpl'),
+        'footer': read_sources('footer.tpl'),
+    }
 
-defaultCommand = 'print(\'Welcome!\')'
-hiddenContext = ('hiddenContext', 'resetTerminal', 'runPython', 'clean', 'precompile', 'execute', 'redirectOutputTo', 'readSources', 'index', 'sources', 'get', 'post', 'request', 'route', 'run', 'template', 'view', 'context', 'StringIO', '__doc__', '__file__', '__name__', '__package__', 'defaultCommand', 'history', 'sys', 'traceback')
-context = {}
-history = []
 
-def resetTerminal():
-    global context
-    context = dict(globals().items() + locals().items())
-    for hiddenEntry in hiddenContext:
-        if hiddenEntry in context:
-            del context[hiddenEntry]
-    global history
-    history = []
+DEFAULT_COMMAND = 'print(\'Welcome!\')'
+HIDDEN_CONTEXT = (
+    'CONTEXT',
+    'DEFAULT_COMMAND',
+    'HIDDEN_CONTEXT',
+    'HISTORY',
+    'StringIO',
+    '__doc__',
+    '__file__',
+    '__name__',
+    '__package__',
+    'clean',
+    'execute',
+    'get',
+    'index',
+    'post',
+    'precompile',
+    'read_sources',
+    'redirect_output_to',
+    'request',
+    'reset_terminal',
+    'route',
+    'run',
+    'run_python',
+    'sources',
+    'sys',
+    'template',
+    'traceback',
+    'view',
+)
+CONTEXT = {}
+HISTORY = []
 
-def runPython(command):
+
+def reset_terminal():
+    global CONTEXT
+    CONTEXT = {**globals(), **locals()}
+    for hiddenEntry in HIDDEN_CONTEXT:
+        if hiddenEntry in CONTEXT:
+            del CONTEXT[hiddenEntry]
+    global HISTORY
+    HISTORY = []
+
+
+def run_python(command):
     buffer = StringIO()
 
     def clean(command):
         ''' Remove Windows-style line ending and make sure single-line expressions evaluate. '''
         return command.replace('\r\n', '\n') + '\n\n'
 
-    @redirectOutputTo(buffer)
+    @redirect_output_to(buffer)
     def precompile(command):
         return compile(command, '<string>', 'single')
 
-    @redirectOutputTo(buffer)
+    @redirect_output_to(buffer)
     def execute(command):
-        exec(command) in context, context
+        exec(command, CONTEXT)
         return
 
     execute(precompile(clean(command)))
-    global history
-    history += [(command, buffer.getvalue())]
-    return history
+    global HISTORY
+    HISTORY += [(command, buffer.getvalue())]
+    return HISTORY
 
-class redirectOutputTo(object):
+
+class redirect_output_to:
     def __init__(self, buffer):
         self.buffer = buffer
 
     def __call__(self, f):
         def wrapper(*args, **kwargs):
-            (out, err, sys.stdout, sys.stderr) = (sys.stdout, sys.stderr, self.buffer, self.buffer)
+            out, err, sys.stdout, sys.stderr = sys.stdout, sys.stderr, self.buffer, self.buffer
             try:
                 return f(*args, **kwargs)
             except:
                 self.buffer.write(traceback.format_exc())
             finally:
-                (sys.stdout, sys.stderr) = (out, err)
+                sys.stdout, sys.stderr = out, err
         return wrapper
 
-def readSources(filename):
-    return ''.join(open(filename,'r').readlines())
+
+def read_sources(filename):
+    return ''.join(open(filename, 'r').readlines())
+
 
 run(host=sys.argv[1], port=sys.argv[2], debug=True)
